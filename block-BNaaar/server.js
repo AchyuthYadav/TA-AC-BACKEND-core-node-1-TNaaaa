4f67af101b0e0665af517b250f975cec0f63bf09
achyuth yadav <achyuthyadav@hmail.com>
2023-01-08 13:17:40 +0530

    assignment

--- block-BNaaar/server.js
+++ block-BNaaar/server.js
@@ -5,12 +5,16 @@ var server = http.createServer(handleRequest);
 function handleRequest (req, res){
     console.log(req.method, req.url);
     if(req.method === 'GET' && req.url === '/') {
+        res.setHeader('Content-Type', 'text/plain')
         res.write('welocome to node');
         res.end();
     } else if(req.method === 'GET' && req.url === '/about'){
         res.setHeader('Content-Type', 'text/html')
         res.end('<h2> about page </h2>')
-    } 
+    } else if(req.method === 'POST' && req.url === '/about'){
+        res.setHeader('Content-Type', 'application/json')
+        res.end('{message: this is a post request}')
+    }
 }
 
 server.listen(3000, () => {
